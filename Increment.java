78219243f9d9d88dd5d448a68b27cb2a6ef919df
Nandini Shrivastav <139245306+Nandini016@users.noreply.github.com>
2023-08-14 21:19:44 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,6 @@
+ class Increment {
+        public static void main(String args[])         {                 
+              int g = 3; 
+            System.out.print(++g * 8);    
+    }   
+  }
---
+++ sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class sum
+{
+ public static void main(String args[])
+ {
+Scanner sc=new Scanner(System.in);
+int  n,sum=0;
+System.out.println("Enter the number you want the sum of:");
+n=sc.nextInt();
+for(int i=1;i<=n;i++)
+{
+ sum=sum+i;
+}
+System.out.println("Sum of "+n+"numbers is="+sum);
+
+
+ } 
+
+}

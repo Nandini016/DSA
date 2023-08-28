bca43f18b5da91d1bd0be2042c0378ac03a11fdd
Nandini Shrivastav <139245306+Nandini016@users.noreply.github.com>
2023-08-28 23:53:26 +0530

    Add files via upload

---
+++ arrayInFunction.java
@@ -0,0 +1,17 @@
+public class arrayInFunction{
+    public static void update( int marks[]){
+        for(int i=0;i<marks.length;i++){
+            marks[i] = marks[i] +1;
+        }
+
+    }
+    public static void main(String args[]){
+        int marks[] ={35,45, 55};
+        update(marks);
+
+        for(int i=0;i<marks.length;i++){
+            System.out.print(marks[i]+ " ");
+        }
+        System.out.println();
+    }
+}

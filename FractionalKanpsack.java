ffc6526d6742547529a7531c30672980bde47c13
Nandini Shrivastav <139245306+Nandini016@users.noreply.github.com>
2024-01-18 22:17:22 +0530

    Add files via upload

---
+++ FractionalKanpsack.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class FractionalKanpsack {
+    public static void main(String args[]){
+        int val[] ={60,100,120};
+        int weight[] ={10,20,30};
+        int W =50;
+
+        double ratio[][]=new double[val.length][2];
+        //0th indx=indx of val ,1st inx =ratio
+        for(int i=0;i<val.length;i++){
+            ratio[i][0] =i;
+            ratio[i][1] =val[i]/(double)weight[i];
+
+        }
+        Arrays.sort(ratio,Comparator.comparingDouble(o->o[1]));
+        int capacity =W;
+        int finalValue =0;
+        for(int i=ratio.length-1;i>=0;i--){
+            int idx =(int)ratio[i][0];
+            if(capacity>=weight[idx]){
+                finalValue+=val[idx];
+                capacity-=weight[idx];
+            }
+            else{
+                finalValue+=(ratio[i][1]*capacity);
+                capacity =0;
+                break;
+            }
+        }
+        System.out.println("final value "+finalValue);
+    }
+    
+}

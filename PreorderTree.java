5bc807826a4bb5f77f8633fd48577de86ef2b841
Nandini Shrivastav <139245306+Nandini016@users.noreply.github.com>
2024-01-22 16:08:41 +0530

    Add files via upload

--- PreorderTree.java
+++ PreorderTree.java
@@ -1,38 +1,45 @@
-public class PreorderTree{
-    static class Node{
+public class PreorderTree {
+    static class Node {
         int data;
         Node left;
         Node right;
-        public Node(int data){
-            this.data =data;
-            this.left =null;
-            this.right=null;
 
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
         }
-
     }
-    public static class BinaryTree{
-            static int idx =-1;
-        public  Node Buildtree(int node[]){
+
+    public static class BinaryTree {
+        static int idx = -1;
+
+        public Node Buildtree(int node[]) {
             idx++;
             if (idx >= node.length || node[idx] == -1) {
                 return null;
             }
             Node newnode = new Node(node[idx]);
-            newnode.left =Buildtree(node);
-            newnode.right= Buildtree(node);
+            newnode.left = Buildtree(node);
+            newnode.right = Buildtree(node);
             return newnode;
-
         }
-        
+
+        public static void preorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");  // Use print instead of println to have a space-separated output
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
-    public static void main(String args[]){
-        int node[] ={1,2,4,-1,-1,5,-1,-1,3,-1,6};
-        BinaryTree tree =new BinaryTree();
-        Node root =tree.Buildtree(node);
-        System.out.println(root);
-         
-       
+
+    public static void main(String args[]) {
+        int node[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.Buildtree(node);
+        System.out.println("Preorder Traversal:");
+        tree.preorder(root);
     }
-   
 }

df7f439a7554299ed6e1c33d66ae54df9c0127d1
Nandini Shrivastav <139245306+Nandini016@users.noreply.github.com>
2024-02-12 21:50:49 +0530

    Add files via upload

---
+++ ItenaryTickets.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class ItenaryTickets {
+    public static String getStart(HashMap<String, String> tickets) {
+        HashMap<String, String> revMap = new HashMap<>();
+        for (String key : tickets.keySet()) {
+            revMap.put(tickets.get(key), key);
+        }
+        for (String key : tickets.keySet()) {
+            if (!revMap.containsKey(key)) {
+                return key; // starting point
+            }
+        }
+        return null;
+    }
+
+    public static void main(String args[]) {
+        HashMap<String, String> tickets = new HashMap<>();
+        tickets.put("Chennai", "Bengaluru");
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Delhi", "Delhi");
+
+        String start = getStart(tickets);
+        System.out.println(start);
+
+        String current = start;
+        while (current != null) {
+            System.out.println(current + " -> " + tickets.get(current));
+            current = tickets.get(current);
+        }
+    }
+}

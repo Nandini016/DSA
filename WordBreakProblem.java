61849a0e11a7b0d4196c985eac3265397118ff14
Nandini Shrivastav <139245306+Nandini016@users.noreply.github.com>
2024-02-14 18:15:48 +0530

    Add files via upload

---
+++ WordBreakProblem.java
@@ -0,0 +1,61 @@
+public class WordBreakProblem {
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow =false;
+        Node(){
+            for(int i=0;i<26;i++){
+                children[i] =null;
+            }
+        }
+    }
+    public static Node root = new Node();//create a root node
+
+    public static void insert(String word){//insert in root node
+        Node curr = root;
+        for(int level=0;level<word.length();level++){
+            int idx =word.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                curr.children[idx] =new Node();
+
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow =true;
+    }
+    public static boolean Search(String key){
+        Node curr = root;
+        for(int level=0;level<key.length();level++){
+            int idx =key.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+         return curr.eow ==true;
+
+    }
+    public static boolean WordBreak(String key){
+        if(key.length()==0){
+            return true;
+        }
+        for(int i=1;i<=key.length();i++){
+            if(Search(key.substring(0,i))&&
+            WordBreak(key.substring(i))){
+                return true;
+
+            }
+
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        String word[]={"i", "like", "sam", "samsung"};
+        for (int i = 0; i < word.length; i++) {
+            insert(word[i]);
+ 
+         }
+         String key ="ilikesamsung";
+         System.out.println(WordBreak(key));
+        }    
+    
+}
